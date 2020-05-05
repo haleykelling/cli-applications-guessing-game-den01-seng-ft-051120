dcb4968d8b9d7d4f62a91b4ffff23c55e9630344
Haley Kelling <haley@kellings.com>
2020-05-05 16:43:53 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@
 def run_guessing_game
   computer_guess = rand(1..6)
   user_guess = gets.chomp
-  if user_guess.to_i == computer_guess
+  if user_guess == computer_guess
     puts "You guessed the correct number!"
   elsif user_guess == "exit"
     puts "Goodbye!"
